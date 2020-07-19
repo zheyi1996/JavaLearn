f3d8a2b63a89a27e8571af254b25e89b3d4b14fc
WeiLiuGuo <305616809@qq.com>
2020-07-19 11:08:58 +0800

    2020-7-19

---
+++ src/main/java/cn/neu/edu/wlg/offer/Offer054.java
@@ -0,0 +1,13 @@
+package cn.neu.edu.wlg.offer;
+
+public class Offer054 {
+
+//    //Insert one char from stringstream
+//    public void Insert(char ch) {
+//
+//    }
+//    //return the first appearence once char in current stringstream
+//    public char FirstAppearingOnce() {
+//
+//    }
+}
