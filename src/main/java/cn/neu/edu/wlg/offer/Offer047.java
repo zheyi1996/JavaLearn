70eda82c1b1b5594e47272ea3777332e6e820fd0
WeiLiuGuo <305616809@qq.com>
2020-07-29 11:03:36 +0800

    2020-7-29

--- src/main/java/cn/neu/edu/wlg/offer/Offer047.java
+++ src/main/java/cn/neu/edu/wlg/offer/Offer047.java
@@ -1,9 +1,15 @@
 package cn.neu.edu.wlg.offer;
 
 
+/*
+    题目：从1加到n
+    思路：使用 pow() 和 二进制右移
+ */
 public class Offer047 {
 
     public int Sum_Solution(int n) {
-        return (1 + n) * n / 2;
+        int sum = (int)(Math.pow(n, 2) + n);
+        return sum >> 1;
+//        return (1 + n) * n / 2;
     }
 }
