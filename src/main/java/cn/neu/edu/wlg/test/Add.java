3ea90f6edda86a3402f996069f3b4a908668e909
zheyi1996 <3054616809@qq.com>
2020-09-12 15:41:43 +0800

    2020-9-12

--- src/main/java/cn/neu/edu/wlg/test/Add.java
+++ src/main/java/cn/neu/edu/wlg/test/Add.java
@@ -1,16 +1,38 @@
-//public int findTheLongestSubstring(String s) {
-//        int res = 0;
+//import java.util.Arrays;
+//
+//public class Main {
+//    public static void main(String[] args){
+//
+//        System.out.println(findTheLongestSubstring("amabc"));
+//    }
+//    public static int findTheLongestSubstring(String s) {
 //        int n = s.length();
-//        Map<Integer, Integer> map = new HashMap<>();
-//        int mask = 0;
+//        int[] pos = new int[1 << 6];
+//        Arrays.fill(pos, -1);
+//        int ans = 0, status = 0;
+//        pos[0] = 0;
 //        for (int i = 0; i < n; i++) {
-//        char c = s.charAt(i);
-//        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-//        mask = mask ^ (1 << (c - 'a'));
+//            char ch = s.charAt(i);
+//            if (ch == 'a') {
+//                status ^= (1 << 0);
+//            } else if (ch == 'b') {
+//                status ^= (1 << 1);
+//            } else if (ch == 'c') {
+//                status ^= (1 << 2);
+//            } else if (ch == 'x') {
+//                status ^= (1 << 3);
+//            } else if (ch == 'y') {
+//                status ^= (1 << 4);
+//            }else if (ch == 'z') {
+//                status ^= (1 << 5);
+//            }
+//            if (pos[status] >= 0) {
+//                ans = Math.max(ans, i + 1 - pos[status]);
+//            } else {
+//                pos[status] = i + 1;
+//            }
 //        }
-//        if (mask == 0) res = Math.max(res, i + 1);
-//        else if (map.containsKey(mask)) res = Math.max(res, i - map.get(mask));
-//        else map.put(mask, i);
-//        }
-//        return res;
-//        }
+//        return ans;
+//    }
+//
+//}
