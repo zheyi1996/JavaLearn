3f482ae6838ec764366a71bade83169b44a57ae5
zheyi1996 <3054616809@qq.com>
2020-09-12 15:36:58 +0800

    2020-9-12

--- src/main/java/cn/neu/edu/wlg/test/Add.java
+++ src/main/java/cn/neu/edu/wlg/test/Add.java
@@ -1,35 +1,16 @@
-//class Solution {
-//    public int minDistance(String word1, String word2) {
-//        int n = word1.length();
-//        int m = word2.length();
-//
-//        // 有一个字符串为空串
-//        if (n * m == 0)
-//            return n + m;
-//
-//        // DP 数组
-//        int [][] D = new int[n + 1][m + 1];
-//
-//        // 边界状态初始化
-//        for (int i = 0; i < n + 1; i++) {
-//            D[i][0] = i;
+//public int findTheLongestSubstring(String s) {
+//        int res = 0;
+//        int n = s.length();
+//        Map<Integer, Integer> map = new HashMap<>();
+//        int mask = 0;
+//        for (int i = 0; i < n; i++) {
+//        char c = s.charAt(i);
+//        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+//        mask = mask ^ (1 << (c - 'a'));
 //        }
-//        for (int j = 0; j < m + 1; j++) {
-//            D[0][j] = j;
+//        if (mask == 0) res = Math.max(res, i + 1);
+//        else if (map.containsKey(mask)) res = Math.max(res, i - map.get(mask));
+//        else map.put(mask, i);
 //        }
-//
-//        // 计算所有 DP 值
-//        for (int i = 1; i < n + 1; i++) {
-//            for (int j = 1; j < m + 1; j++) {
-//                int left = D[i - 1][j] + 1;
-//                int down = D[i][j - 1] + 1;
-//                int left_down = D[i - 1][j - 1];
-//                if (word1.charAt(i - 1) != word2.charAt(j - 1))
-//                    left_down += 1;
-//                D[i][j] = Math.min(left, Math.min(down, left_down));
-//
-//            }
+//        return res;
 //        }
-//        return D[n][m];
-//    }
-//}
